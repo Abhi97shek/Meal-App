802d32b7bcffe885ea8e1ac93f0469ff35ce3eec
Abhi97shek <abhi.kumar736@gmail.com>
2022-01-07 23:56:15 +0530

    Grid Styling Done

--- screens/CategoriesScreen.js
+++ screens/CategoriesScreen.js
@@ -1,5 +1,5 @@
 import React from "react";
-import {View,StyleSheet,Text,Button,TouchableOpacity,Platform} from 'react-native';
+import {View,StyleSheet,Text,Button,TouchableOpacity,Platform,TouchableNativeFeedback} from 'react-native';
 import { FlatList } from "react-native-gesture-handler";
 import { CATEGORIES } from "../data/dummy-data";
 import Colors from "../constants/Colors";
@@ -11,13 +11,15 @@ const CategoriesScreen = (props)=>{
     const renderGridItem=(itemData)=>{
 
         return (
-            <TouchableOpacity style={styles.gridItems} onPress={()=>props.navigation.navigate('CategoryMeal',{
+            <View style={styles.gridItems}>
+            <TouchableNativeFeedback  onPress={()=>props.navigation.navigate('CategoryMeal',{
                 categoryId: itemData.item.id
             })}>  
-                  <View>
-                    <Text>{itemData.item.title}</Text>
+                  <View style={{...styles.containerMeal,...{backgroundColor:itemData.item.color}}}>
+                    <Text style={styles.title} numberOfLines={2}>{itemData.item.title}</Text>
                 </View>
-            </TouchableOpacity>
+            </TouchableNativeFeedback>
+            </View>
     
         )
     };
@@ -42,6 +44,22 @@ const styles = StyleSheet.create({
         flex:1,
         margin:15,
         height:150
+    },
+    containerMeal:{
+        flex:1,
+        borderRadius:10,
+        shadowColor:'black',
+        shadowOpacity:0.26,
+        shadowOffset:{width:0,height:2},
+        shadowRadius:10,
+        elevation:3,
+        padding:10,
+        justifyContent:'flex-end',
+        alignItems:'flex-end'
+    },
+    title:{
+        
+        fontSize:16
     }
 
 });
