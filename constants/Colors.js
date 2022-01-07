02dc0c944ef89704027da6bea5122e566bceeae5
Abhi97shek <abhi.kumar736@gmail.com>
2022-01-07 22:07:04 +0530

    Add Navigation Header

---
+++ constants/Colors.js
@@ -0,0 +1,4 @@
+export default {
+    primaryColor : '#4a14bc',
+    accentColor: '#ff6f00'
+}
--- screens/CategoriesScreen.js
+++ screens/CategoriesScreen.js
@@ -1,24 +1,38 @@
 import React from "react";
-import {View,StyleSheet,Text,Button} from 'react-native';
+import {View,StyleSheet,Text,Button,TouchableOpacity,Platform} from 'react-native';
 import { FlatList } from "react-native-gesture-handler";
 import { CATEGORIES } from "../data/dummy-data";
+import Colors from "../constants/Colors";
 
 
-const renderGridItem=(itemData)=>{
-
-    return (
-    <View style={styles.gridItems}>
-        <Text>{itemData.item.title}</Text>
-    </View>
-    )
-};
 const CategoriesScreen = (props)=>{
 
+
+    const renderGridItem=(itemData)=>{
+
+        return (
+            <TouchableOpacity style={styles.gridItems} onPress={()=>props.navigation.navigate('CategoryMeal')}>  
+                  <View>
+                    <Text>{itemData.item.title}</Text>
+                </View>
+            </TouchableOpacity>
+    
+        )
+    };
+    
     return (
        <FlatList data={CATEGORIES} renderItem={renderGridItem} numColumns={2}/>
     )
 };
+CategoriesScreen.navigationOptions = {
+    title: 'Meal Categories',
+    headerStyle:{
+        backgroundColor: Platform.OS === 'android' ? Colors.primaryColor : 'white'
+    },
+    headerTintColor: Platform.OS === 'android' ? 'white' : Colors.primaryColor
 
+
+};
 const styles = StyleSheet.create({
     screen:{
         flex:1,
